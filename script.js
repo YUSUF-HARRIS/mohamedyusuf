45a2cdc7c8a240e66c71969da1cb53989bbfe353
mohamed yusuf <mohamedyusuf0445@gmail.com>
2025-04-23 16:04:05 +0530

    chnaged

--- script.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed("#typed", {
-    strings: ["I am a web developer", "I love building websites", "Let's create something amazing!"],
-    typeSpeed: 50,
-    backSpeed: 30,
-    loop: true
-  });
-  
